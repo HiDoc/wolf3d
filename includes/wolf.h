f6a0ec6828f5bcdec3776a4d5e77a80e463fff64
HiDoc <fmadura@student.42.fr>
2019-02-23 14:09:07 +0100

    struct

--- includes/wolf.h
+++ includes/wolf.h
@@ -6,7 +6,7 @@
 /*   By: fmadura <fmadura@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 11:52:53 by fmadura           #+#    #+#             */
-/*   Updated: 2019/02/23 13:39:47 by sgalasso         ###   ########.fr       */
+/*   Updated: 2019/02/23 14:02:42 by fmadura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define WOLF_H
 # include "wolf_define.h"
 # include "wolf_struct.h"
+# include "doom_engine.h"
 
 #endif
