091639aa8894850b832ed5f9ca4f6eb4aeebfb20
HiDoc <fmadura@student.42.fr>
2019-02-25 14:00:18 +0100

    draw

--- srcs/draw.c
+++ srcs/draw.c
@@ -24,6 +24,86 @@ static void				vline(t_line l, t_engine *e)
 	}
 }
 
+/* If it's partially behind the player, clip it against player's view frustrum */
+void	clip_view(t_vertex *t)
+{
+	const float nearz = 1e-4f;
+	const float farz = 5;
+	const float nearside = 1e-5f;
+	const float farside = 20.f;
+	// Find an intersection between the wall and the approximate edges of player's view
+	t_xy i1 = Intersect(t->x1,t->y1,t->x2,t->y2, -nearside,nearz, -farside,farz);
+	t_xy i2 = Intersect(t->x1,t->y1,t->x2,t->y2,  nearside,nearz,  farside,farz);
+	if (t->y1 < nearz)
+	{
+		if (i1.y > 0)
+		{
+			t->x1 = i1.x;
+			t->y1 = i1.y;
+		}
+		else
+		{
+			t->x1 = i2.x;
+			t->y1 = i2.y;
+		}
+	}
+	if (t->y2 < nearz)
+	{
+		if (i1.y > 0)
+		{
+			t->x2 = i1.x;
+			t->y2 = i1.y;
+		}
+		else
+		{
+			t->x2 = i2.x;
+			t->y2 = i2.y;
+		}
+	}
+}
+
+void	render_wall(t_vertex t, t_projec p, t_projec n, int x, int x1, int x2, int *ytop, int *ybottom, t_engine *e, int neighbor)
+{
+	/* Calculate the Z coordinate for this point. (Only used for lighting.) */
+	int z = ((x - x1) * (t.y2 - t.y1) / (x2 - x1) + t.y1) * 8;
+	/* Acquire the Y coordinates for our ceiling & floor for this X coordinate. Clamp them. */
+	int ya = (x - x1) * (p.y2a - p.y1a) / (x2 - x1) + p.y1a;
+	int cya = clamp(ya, *ytop, *ybottom); // top
+	int yb = (x - x1) * (p.y2b - p.y1b) / (x2 - x1) + p.y1b;
+	int cyb = clamp(yb, *ytop, *ybottom); // bottom
+
+	/* Render ceiling: everything above this sector's ceiling height. */
+	vline((t_line){x, *ytop, cya - 1, 0x111111 , 0x222222, 0x111111}, e);
+
+	/* Render floor: everything below this sector's floor height. */
+	vline((t_line){x, cyb + 1, *ybottom, 0x0000FF,0x0000AA,0x0000FF}, e);
+
+	/* Is there another sector behind this edge? */
+	if (neighbor >= 0)
+	{
+		/* Same for _their_ floor and ceiling */
+		int nya = (x - x1) * (n.y2a - n.y1a) / (x2 - x1) + n.y1a;
+		int	cnya = clamp(nya, *ytop,*ybottom);
+		int nyb = (x - x1) * (n.y2b-n.y1b) / (x2 - x1) + n.y1b;
+		int cnyb = clamp(nyb, *ytop,*ybottom);
+
+		/* If our ceiling is higher than their ceiling, render upper wall */
+		unsigned r1 = 0x010101 * (255-z), r2 = 0x040007 * (31-z/8);
+		vline((t_line){x, cya, cnya-1, 0, x==x1 || x==x2 ? 0 : r1, 0}, e); // Between our and their ceiling
+		*ytop = clamp(max(cya, cnya), *ytop, H - 1);   // Shrink the remaining window below these ceilings
+
+		/* If our floor is lower than their floor, render bottom wall */
+		vline((t_line){x, cnyb + 1, cyb, 0, x==x1 || x==x2 ? 0 : r2, 0}, e); // Between their and our floor
+		*ybottom = clamp(min(cyb, cnyb), 0, *ybottom); // Shrink the remaining window above these floors
+	}
+	else
+	{
+		/* There's no neighbor. Render wall from top (cya = ceiling level) to bottom (cyb = floor level). */
+		unsigned r = 0x010101 * (255 - z);
+		vline((t_line){x, cya, cyb, 0, x==x1 || x==x2 ? 0 : r, 0}, e);
+	}
+}
+
 void    DrawScreen(t_engine *e)
 {
 	enum { MaxQueue = 32 };  // maximum number of pending portal renders
@@ -80,42 +160,10 @@ void    DrawScreen(t_engine *e)
 			};
 			/* Is the wall at least partially in front of the player? */
 			if (t.y1 <= 0 && t.y2 <= 0)
-				continue;
+				continue ;
 			/* If it's partially behind the player, clip it against player's view frustrum */
 			if (t.y1 <= 0 || t.y2 <= 0)
-			{
-				float nearz = 1e-4f;
-				float farz = 5;
-				float nearside = 1e-5f;
-				float farside = 20.f;
-				// Find an intersection between the wall and the approximate edges of player's view
-				t_xy i1 = Intersect(t.x1,t.y1,t.x2,t.y2, -nearside,nearz, -farside,farz);
-				t_xy i2 = Intersect(t.x1,t.y1,t.x2,t.y2,  nearside,nearz,  farside,farz);
-				if (t.y1 < nearz) {
-					if (i1.y > 0)
-					{
-						t.x1 = i1.x;
-						t.y1 = i1.y;
-					}
-					else
-					{
-						t.x1 = i2.x;
-						t.y1 = i2.y;
-					}
-				}
-				if (t.y2 < nearz) {
-					if (i1.y > 0)
-					{
-						t.x2 = i1.x;
-						t.y2 = i1.y;
-					}
-					else
-					{
-						t.x2 = i2.x;
-						t.y2 = i2.y;
-					}
-				}
-			}
+				clip_view(&t);
 			/* Do perspective transformation */
 			t_vertex scale = (t_vertex){
 				hfov / t.y1,
@@ -133,11 +181,11 @@ void    DrawScreen(t_engine *e)
 			/* Acquire the floor and ceiling heights, relative to where the player's view is */
 			float yceil  = sect->ceil  - e->player.where.z;
 			float yfloor = sect->floor - e->player.where.z;
+			float nyceil = 0;
+			float nyfloor = 0;
 
 			/* Check the edge type. neighbor=-1 means wall, other=boundary between two e->sectors. */
 			int neighbor = sect->neighbors[s];
-			float nyceil = 0;
-			float nyfloor = 0;
 
 			if (neighbor >= 0) // Is another sector showing through this portal?
 			{
@@ -147,59 +195,26 @@ void    DrawScreen(t_engine *e)
 
 			/* Project our ceiling & floor heights into screen coordinates (Y coordinate) */
 			#define Yaw(y,z) (y + z * e->player.yaw)
-			int y1a = H/2 - (int)(Yaw(yceil, t.y1) * scale.y1);
-			int y1b = H/2 - (int)(Yaw(yfloor, t.y1) * scale.y1);
-			int y2a = H/2 - (int)(Yaw(yceil, t.y2) * scale.y2);
-			int y2b = H/2 - (int)(Yaw(yfloor, t.y2) * scale.y2);
-			/* The same for the neighboring sector */
-			int ny1a = H/2 - (int)(Yaw(nyceil, t.y1) * scale.y1);
-			int ny1b = H/2 - (int)(Yaw(nyfloor, t.y1) * scale.y1);
-			int ny2a = H/2 - (int)(Yaw(nyceil, t.y2) * scale.y2);
-			int ny2b = H/2 - (int)(Yaw(nyfloor, t.y2) * scale.y2);
-
+			t_projec p = (t_projec){
+				H/2 - (int)(Yaw(yceil, t.y1) * scale.y1),
+				H/2 - (int)(Yaw(yfloor, t.y1) * scale.y1),
+				H/2 - (int)(Yaw(yceil, t.y2) * scale.y2),
+				H/2 - (int)(Yaw(yfloor, t.y2) * scale.y2)
+			};
+			t_projec n = (t_projec){
+				H/2 - (int)(Yaw(nyceil, t.y1) * scale.y1),
+				H/2 - (int)(Yaw(nyfloor, t.y1) * scale.y1),
+				H/2 - (int)(Yaw(nyceil, t.y2) * scale.y2),
+				H/2 - (int)(Yaw(nyfloor, t.y2) * scale.y2)
+			};
 			/* Render the wall. */
 			int beginx = max(x1, now.sx1);
 			int endx = min(x2, now.sx2);
-			for (int x = beginx; x <= endx; ++x)
+			int x = beginx;
+			while (x <= endx)
 			{
-				/* Calculate the Z coordinate for this point. (Only used for lighting.) */
-				int z = ((x - x1) * (t.y2 - t.y1) / (x2 - x1) + t.y1) * 8;
-				/* Acquire the Y coordinates for our ceiling & floor for this X coordinate. Clamp them. */
-				int ya = (x - x1) * (y2a - y1a) / (x2 - x1) + y1a;
-				int cya = clamp(ya, ytop[x], ybottom[x]); // top
-				int yb = (x - x1) * (y2b - y1b) / (x2 - x1) + y1b;
-				int cyb = clamp(yb, ytop[x], ybottom[x]); // bottom
-
-				/* Render ceiling: everything above this sector's ceiling height. */
-				vline((t_line){x, ytop[x], cya - 1, 0x111111 , 0x222222, 0x111111}, e);
-
-				/* Render floor: everything below this sector's floor height. */
-				vline((t_line){x, cyb + 1, ybottom[x], 0x0000FF,0x0000AA,0x0000FF}, e);
-
-				/* Is there another sector behind this edge? */
-				if (neighbor >= 0)
-				{
-					/* Same for _their_ floor and ceiling */
-					int nya = (x - x1) * (ny2a - ny1a) / (x2 - x1) + ny1a;
-					int	cnya = clamp(nya, ytop[x],ybottom[x]);
-					int nyb = (x - x1) * (ny2b-ny1b) / (x2 - x1) + ny1b;
-					int cnyb = clamp(nyb, ytop[x],ybottom[x]);
-
-					/* If our ceiling is higher than their ceiling, render upper wall */
-					unsigned r1 = 0x010101 * (255-z), r2 = 0x040007 * (31-z/8);
-					vline((t_line){x, cya, cnya-1, 0, x==x1 || x==x2 ? 0 : r1, 0}, e); // Between our and their ceiling
-					ytop[x] = clamp(max(cya, cnya), ytop[x], H - 1);   // Shrink the remaining window below these ceilings
-
-					/* If our floor is lower than their floor, render bottom wall */
-					vline((t_line){x, cnyb + 1, cyb, 0, x==x1 || x==x2 ? 0 : r2, 0}, e); // Between their and our floor
-					ybottom[x] = clamp(min(cyb, cnyb), 0, ybottom[x]); // Shrink the remaining window above these floors
-				}
-				else
-				{
-					/* There's no neighbor. Render wall from top (cya = ceiling level) to bottom (cyb = floor level). */
-					unsigned r = 0x010101 * (255 - z);
-					vline((t_line){x, cya, cyb, 0, x==x1 || x==x2 ? 0 : r, 0}, e);
-				}
+				render_wall(t, p, n, x, x1, x2, &ytop[x], &ybottom[x], e, neighbor);
+				x++;
 			}
 			/* Schedule the neighboring sector for rendering within the window formed by this wall. */
 			if (neighbor >= 0 && endx >= beginx && (head+MaxQueue + 1 - tail) % MaxQueue)
