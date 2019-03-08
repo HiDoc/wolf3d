8060e5fa9b51719990f3b30382074f430eb0336d
Cha <annecharlottebaille@gmail.com>
2019-03-08 20:05:24 +0100

    hud 1.1

--- srcs/struct_hud.c
+++ srcs/struct_hud.c
@@ -7,13 +7,13 @@ int init_hud(t_env *env)
     i = 0;
     while (i < 3)
         env->player.hud.shortcut[i++] = NULL;
-    if ((env->player.hud.empty_bar = ui_img("hud/empty_bar3"))
+    if ((env->player.hud.empty_bar = ui_img("hud/empty_bar"))
     && (env->player.hud.bar[0] = ui_img("hud/health_bar"))
-    && (env->player.hud.bar[1] = ui_img("hud/shield_bar3"))
+    && (env->player.hud.bar[1] = ui_img("hud/shield_bar"))
     && (env->player.hud.hud_wpn[0] = ui_img("hud/hud_pistol"))
     && (env->player.hud.hud_wpn[1] = ui_img("hud/hud_ak"))
     && (env->player.hud.hud_wpn[2] = ui_img("hud/hud_rifle"))
-    && (env->player.hud.pad = ui_img("hud/pad2")))
+    && (env->player.hud.pad = ui_img("hud/pad")))
         return (1);
     return (0);
 }
