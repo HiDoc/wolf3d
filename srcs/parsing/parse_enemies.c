3058cb5ff847e4790d76b689c078f11ffa93031c
Cha <annecharlottebaille@gmail.com>
2019-04-04 23:33:13 +0200

    stats + god mod + velocity bullet + stuf

--- srcs/parsing/parse_enemies.c
+++ srcs/parsing/parse_enemies.c
@@ -6,7 +6,7 @@
 /*   By: abaille <abaille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 11:44:22 by abaille           #+#    #+#             */
-/*   Updated: 2019/04/04 21:38:50 by abaille          ###   ########.fr       */
+/*   Updated: 2019/04/04 23:32:52 by abaille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,18 +58,9 @@ int	fill_enemies_sector(t_env *env, t_sector *sector, t_vtx v, int ref)
 
 	iter = NULL;
 	if (sector->head_enemy == NULL)
-	{
-		if (!(sector->head_enemy = new_enemy(env, v, ref)))
-			return (0);
-	}
-	else
-	{
-		iter = sector->head_enemy;
-		while (iter->next != NULL)
-			iter = iter->next;
-		if (!(iter->next = new_enemy(env, v, ref)))
-			return (0);
-	}
-	sector->nb_enemies++;
-	return (1);
+		return ((sector->head_enemy = new_enemy(env, v, ref)) ? sector->nb_enemies++ : 0);
+	iter = sector->head_enemy;
+	while (iter->next != NULL)
+		iter = iter->next;
+	return ((iter->next = new_enemy(env, v, ref)) ? sector->nb_enemies++ : 0);
 }
