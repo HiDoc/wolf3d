73d5133c752f440655d5229706f8e6523f4cea47
Simon Galasso <sgalasso@student.42.fr>
2019-04-21 17:16:02 +0200

    [SG] [skybox] refactor + opti skybox

--- srcs/display_skybox.c
+++ srcs/display_skybox.c
@@ -6,43 +6,59 @@
 /*   By: sgalasso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 12:56:28 by sgalasso          #+#    #+#             */
-/*   Updated: 2019/04/12 22:06:43 by sgalasso         ###   ########.fr       */
+/*   Updated: 2019/04/21 17:14:45 by sgalasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom.h"
 
-void		display_skybox(t_env *env)
+static Uint32	get_sb_color(int x, int y, float roll_d, float yaw_d, t_env *env)
 {
-	int				w;
-	int				h;
-	float			player_d;
+	float		wcalc;
+	float		hcalc;
+
+	wcalc = (x * (env->skybox.sb->w / 4)) / W;
+	wcalc += (roll_d * env->skybox.sb->w) / 360;
+	wcalc = (int)wcalc % env->skybox.sb->w;
+
+	hcalc = H;
+	hcalc += (int)((y / 4) % env->skybox.sb->h);
+	hcalc += (yaw_d * env->skybox.sb->h / 2) / 360;
+	hcalc = (int)hcalc % env->skybox.sb->h;
+
+	return (getpixel(env->skybox.sb, wcalc, hcalc));
+}
+
+void			display_skybox(t_drawline l, t_env *env)
+{
+	int     *pixels;
+	int     x;
+	int     iter;
+	// faire ailleurs pour opti //
+	float			roll_d;
 	float			yaw_d;
-	float			wcalc;
-	float			hcalc;
-
-	player_d = (int)(env->engine.player.angle * 180 / M_PI) % 360;
-	(player_d < 0) ? player_d = 360 + player_d : 0;
-
+	roll_d = (int)(env->engine.player.angle * 180 / M_PI) % 360;
+	(roll_d < 0) ? roll_d = 360 + roll_d : 0;
 	yaw_d = (int)(env->engine.player.yaw * 180 / M_PI);
+	//////////////////
 
-	w = 0;
-	while (w < W)
+	x = ((t_raycast *)l.container)->x;
+	pixels  = (int *)env->sdl.surface->pixels;
+	l.from = clamp(l.from, 0, H - 1);
+	l.to = clamp(l.to, 0, H - 1);
+	if (l.from == l.to)
+		pixels[l.from * W + x] = 0x00000000;
+	else if (l.to > l.from)
 	{
-		wcalc = (w * (env->skybox.sb->w / 4)) / W;
-		wcalc += (player_d * env->skybox.sb->w) / 360;
-		wcalc = (int)wcalc % env->skybox.sb->w;
-		h = 0;
-		while (h < H)
+		pixels[l.from * W + x] = 0xff00ffff;
+		iter = l.from + 1;
+		int y = 0;
+		while (iter < l.to)
 		{
-			hcalc = H;
-			hcalc += (int)((h / 4) % env->skybox.sb->h);
-			hcalc += (yaw_d * env->skybox.sb->h / 2) / 360;
-			hcalc = (int)hcalc % env->skybox.sb->h;
-			setpixel(env->sdl.surface, w, h,
-			getpixel(env->skybox.sb, wcalc, hcalc));
-			h++;
+			pixels[iter * W + x] = get_sb_color(x, iter, roll_d, yaw_d, env);
+			y++;
+			iter++;
 		}
-		w++;
+		pixels[l.to * W + x] = 0x00000000;
 	}
 }
