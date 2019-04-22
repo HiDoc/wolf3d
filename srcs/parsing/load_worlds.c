876056f397e76f9b0fd2cb40bf3a432edd3f7b97
Simon Galasso <sgalasso@student.42.fr>
2019-04-22 19:22:04 +0200

    [SG] [world_loop] new game world merge

--- srcs/parsing/load_worlds.c
+++ srcs/parsing/load_worlds.c
@@ -6,7 +6,7 @@
 /*   By: sgalasso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 14:46:02 by sgalasso          #+#    #+#             */
-/*   Updated: 2019/04/22 18:21:05 by sgalasso         ###   ########.fr       */
+/*   Updated: 2019/04/22 19:18:21 by sgalasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ void		load_worlds(t_env *env)
 		doom_error_exit("Doom_nukem: fd error in load_worlds");
 	if (get_next_line(fd, &line) == -1)
 		doom_error_exit("Doom_nukem: gnl error in load_worlds");
-	env->games = ft_memalloc(sizeof(char *) * (ft_atoi(line) + 1));
+	env->nb_games = ft_atoi(line);
+	env->games = ft_memalloc(sizeof(char *) * (env->nb_games + 1));
 	lt_release(line);
 	i = 0;
 	while (get_next_line(fd, &line) > 0)
