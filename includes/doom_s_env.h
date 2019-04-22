cd1e00c337a6b744021f75c85fa3bd32c509b106
Simon Galasso <sgalasso@student.42.fr>
2019-04-22 14:36:39 +0200

    [SG] [world_loop] mainmenu extraction + archi

--- includes/doom_s_env.h
+++ includes/doom_s_env.h
@@ -6,7 +6,7 @@
 /*   By: abaille <abaille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 11:54:38 by fmadura           #+#    #+#             */
-/*   Updated: 2019/04/22 13:21:46 by sgalasso         ###   ########.fr       */
+/*   Updated: 2019/04/22 13:54:31 by sgalasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,6 +105,9 @@ struct								s_env
 
 	/* skybox */
 	t_skybox		skybox;
+
+	/* world names */
+	char			**games;
 
 	// Comment je penses que ce serait mieux :
 	//
