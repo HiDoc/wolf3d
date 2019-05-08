2b20fe17a135259cb0ee52c70a8f90fda886360f
Florian MADURA <fmadura@e1r8p16.42.fr>
2019-05-08 23:53:22 +0200

    norme v1

--- srcs/engine/struct_cwall.c
+++ srcs/engine/struct_cwall.c
@@ -6,11 +6,75 @@
 /*   By: fmadura <fmadura@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 23:37:09 by fmadura           #+#    #+#             */
-/*   Updated: 2019/05/08 23:38:04 by fmadura          ###   ########.fr       */
+/*   Updated: 2019/05/08 23:52:56 by fmadura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom.h"
+
+void	cwall_loop(t_cwall cwall, int *pixels, SDL_Surface *sprite,
+		t_drawline l)
+{
+	t_raycast	*ctn;
+
+	ctn = ((t_raycast *)l.container);
+	cwall.height += cwall.d;
+	cwall.y += cwall.d;
+	while (cwall.iter < l.to)
+	{
+		cwall.pos = (cwall.y / cwall.height * cwall.scaley) * sprite->h;
+		pixels[cwall.iter * W + ctn->x] = getpixel(sprite, cwall.x
+			% sprite->w, cwall.pos % sprite->h);
+		cwall.y++;
+		cwall.iter++;
+	}
+}
+
+void	calc_cwall(t_cwall *cwall, t_drawline l, t_raycast *ctn)
+{
+	cwall->p = ctn->p;
+	cwall->iter = l.from + 1;
+	cwall->y = 0;
+	cwall->scaley = (ctn->li_sector.ceil - ctn->li_sector.floor) / 20;
+	cwall->d = ((cwall->p.y2a - cwall->p.y1a)) * ((ctn->x2 - ctn->x1)
+		- (ctn->x - ctn->x1)) / (ctn->x2 - ctn->x1) - cwall->p.y2a;
+	cwall->x = (ctn->li_texture.floor * ((ctn->x2 - ctn->x) * ctn->rot.v2.y)
+		+ ctn->li_texture.ceil * ((ctn->x - ctn->x1) * ctn->rot.v1.y))
+		/ ((ctn->x2 - ctn->x) * ctn->rot.v2.y + (ctn->x - ctn->x1)
+		* ctn->rot.v1.y);
+}
+
+void	cwall_loop2(t_cwall cwall, int *pixels, SDL_Surface *sprite,
+		t_drawline l)
+{
+	t_raycast	*ctn;
+
+	ctn = ((t_raycast *)l.container);
+	while (cwall.iter < l.to)
+	{
+		cwall.pos = (cwall.y / cwall.height * cwall.scaley) * sprite->h;
+		pixels[cwall.iter * W + ctn->x] = getpixel(sprite, cwall.x
+			% sprite->w, cwall.pos % sprite->h);
+		cwall.y++;
+		cwall.iter++;
+	}
+}
+
+void	cwall_loop1(t_cwall cwall, int *pixels, SDL_Surface *sprite,
+		t_drawline l)
+{
+	t_raycast	*ctn;
+
+	ctn = ((t_raycast *)l.container);
+	while (cwall.iter < l.to)
+	{
+		cwall.pos = (cwall.y / cwall.height * cwall.scaley) * sprite->h;
+		pixels[cwall.iter * W + ctn->x] = getpixel(sprite, cwall.x
+			% sprite->w, cwall.pos % sprite->h);
+		cwall.y++;
+		cwall.iter++;
+	}
+}
 
 void	render_cwall(t_drawline l, t_env *env)
 {
@@ -20,7 +84,7 @@ void	render_cwall(t_drawline l, t_env *env)
 	t_cwall		cwall;
 
 	ctn = ((t_raycast *)l.container);
-	pixels	= (int *)env->sdl.surface->pixels;
+	pixels = (int *)env->sdl.surface->pixels;
 	sprite = ctn->sprite;
 	cwall.height = l.to - l.from;
 	cwall.iter = 0;
@@ -29,60 +93,13 @@ void	render_cwall(t_drawline l, t_env *env)
 	if (l.from == l.to)
 		pixels[l.from * W + ctn->x] = 0xffffffff;
 	else if (ctn->x == ctn->x1 || ctn->x == ctn->x2)
-	{
-		cwall.iter = l.from + 1;
-		while (cwall.iter < l.to)
-		{
-			pixels[cwall.iter * W + ctn->x] = 0xffffffff;
-			cwall.iter++;
-		}
-	}
+		cwall_loop1(cwall, pixels, sprite, l);
 	else if (l.to > l.from)
 	{
-		cwall.p = ctn->p;
-		pixels[l.from * W + ctn->x] = 0xffffffff;
-		cwall.iter = l.from + 1;
-		cwall.y = 0;
-		cwall.scaley = (ctn->li_sector.ceil - ctn->li_sector.floor) / 20;
-		cwall.d = ((cwall.p.y2a - cwall.p.y1a)) * ((ctn->x2 - ctn->x1) - (ctn->x - ctn->x1))
-			/ (ctn->x2 - ctn->x1) - cwall.p.y2a;
-		cwall.x = (ctn->li_texture.floor * ((ctn->x2 - ctn->x) * ctn->rot.v2.y)
-			+ ctn->li_texture.ceil * ((ctn->x - ctn->x1) * ctn->rot.v1.y))
-			/ ((ctn->x2 - ctn->x) * ctn->rot.v2.y + (ctn->x - ctn->x1) * ctn->rot.v1.y);
-		if (cwall.p.y1a < 0 && l.from == 0)
-		{
-			cwall.height += cwall.d;
-			cwall.y += cwall.d;
-			while (cwall.iter < l.to)
-			{
-				cwall.pos = (cwall.y / cwall.height * cwall.scaley) * sprite->h;
-				pixels[cwall.iter * W + ctn->x] = getpixel(sprite, cwall.x % sprite->w, cwall.pos % sprite->h);
-				cwall.y++;
-				cwall.iter++;
-			}
-		}
-		else if (cwall.p.y2a < 0 && l.from == 0)
-		{
-			cwall.height += cwall.d;
-			cwall.y += cwall.d;
-			while (cwall.iter < l.to)
-			{
-				cwall.pos = (cwall.y / cwall.height * cwall.scaley) * sprite->h;
-				pixels[cwall.iter * W + ctn->x] = getpixel(sprite, cwall.x % sprite->w, cwall.pos % sprite->h);
-				cwall.y++;
-				cwall.iter++;
-			}
-		}
+		calc_cwall(&cwall, l, ctn);
+		if (cwall.p.y1a < 0 && l.from == 0 || cwall.p.y2a < 0 && l.from == 0)
+			cwall_loop(cwall, pixels, sprite, l);
 		else
-		{
-			while (cwall.iter < l.to)
-			{
-				cwall.pos = (cwall.y / cwall.height * cwall.scaley) * sprite->h;
-				pixels[cwall.iter * W + ctn->x] = getpixel(sprite, cwall.x % sprite->w, cwall.pos % sprite->h);
-				cwall.y++;
-				cwall.iter++;
-			}
-		}
-		pixels[l.to * W + ctn->x] = 0xffffffff;
+			cwall_loop2(cwall, pixels, sprite, l);
 	}
 }
--- srcs/engine/struct_drawline.c
+++ srcs/engine/struct_drawline.c
@@ -6,7 +6,7 @@
 /*   By: fmadura <fmadura@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 18:51:15 by fmadura           #+#    #+#             */
-/*   Updated: 2019/05/08 23:36:31 by fmadura          ###   ########.fr       */
+/*   Updated: 2019/05/08 23:53:04 by fmadura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	vline(t_drawline l, t_env *env)
 	int		y;
 
 	x = ((t_raycast *)l.container)->x;
-	pixels	= (int *)env->sdl.surface->pixels;
+	pixels = (int *)env->sdl.surface->pixels;
 	l.from = clamp(l.from, 0, H - 1);
 	l.to = clamp(l.to, 0, H - 1);
 	if (l.from == l.to)
@@ -41,92 +41,6 @@ void	vline(t_drawline l, t_env *env)
 			iter++;
 		}
 		pixels[l.to * W + x] = 0xff00ffff;
-	}
-}
-typedef struct s_cwall
-{
-	int			iter;
-	float		height;
-	int			x;
-	float		y;
-	float		scaley;
-	float		d;
-	int			pos;
-	t_projec	p;
-}t_cwall;
-
-void	render_cwall(t_drawline l, t_env *env)
-{
-	t_raycast	*ctn;
-	int			*pixels;
-	SDL_Surface	*sprite;
-	t_cwall		cwall;
-
-	ctn = ((t_raycast *)l.container);
-	pixels	= (int *)env->sdl.surface->pixels;
-	sprite = ctn->sprite;
-	cwall.height = l.to - l.from;
-	cwall.iter = 0;
-	l.from = clamp(l.from, 0, H - 1);
-	l.to = clamp(l.to, 0, H - 1);
-	if (l.from == l.to)
-		pixels[l.from * W + ctn->x] = 0xffffffff;
-	else if (ctn->x == ctn->x1 || ctn->x == ctn->x2)
-	{
-		cwall.iter = l.from + 1;
-		while (cwall.iter < l.to)
-		{
-			pixels[cwall.iter * W + ctn->x] = 0xffffffff;
-			cwall.iter++;
-		}
-	}
-	else if (l.to > l.from)
-	{
-		cwall.p = ctn->p;
-		pixels[l.from * W + ctn->x] = 0xffffffff;
-		cwall.iter = l.from + 1;
-		cwall.y = 0;
-		cwall.scaley = (ctn->li_sector.ceil - ctn->li_sector.floor) / 20;
-		cwall.d = ((cwall.p.y2a - cwall.p.y1a)) * ((ctn->x2 - ctn->x1) - (ctn->x - ctn->x1))
-			/ (ctn->x2 - ctn->x1) - cwall.p.y2a;
-		cwall.x = (ctn->li_texture.floor * ((ctn->x2 - ctn->x) * ctn->rot.v2.y)
-			+ ctn->li_texture.ceil * ((ctn->x - ctn->x1) * ctn->rot.v1.y))
-			/ ((ctn->x2 - ctn->x) * ctn->rot.v2.y + (ctn->x - ctn->x1) * ctn->rot.v1.y);
-		if (cwall.p.y1a < 0 && l.from == 0)
-		{
-			cwall.height += cwall.d;
-			cwall.y += cwall.d;
-			while (cwall.iter < l.to)
-			{
-				cwall.pos = (cwall.y / cwall.height * cwall.scaley) * sprite->h;
-				pixels[cwall.iter * W + ctn->x] = getpixel(sprite, cwall.x % sprite->w, cwall.pos % sprite->h);
-				cwall.y++;
-				cwall.iter++;
-			}
-		}
-		else if (cwall.p.y2a < 0 && l.from == 0)
-		{
-			cwall.height += cwall.d;
-			cwall.y += cwall.d;
-			while (cwall.iter < l.to)
-			{
-				cwall.pos = (cwall.y / cwall.height * cwall.scaley) * sprite->h;
-				pixels[cwall.iter * W + ctn->x] = getpixel(sprite, cwall.x % sprite->w, cwall.pos % sprite->h);
-				cwall.y++;
-				cwall.iter++;
-			}
-		}
-		else
-		{
-			while (cwall.iter < l.to)
-			{
-				cwall.pos = (cwall.y / cwall.height * cwall.scaley) * sprite->h;
-				pixels[cwall.iter * W + ctn->x] = getpixel(sprite, cwall.x % sprite->w, cwall.pos % sprite->h);
-				cwall.y++;
-				cwall.iter++;
-			}
-		}
-		pixels[l.to * W + ctn->x] = 0xffffffff;
 	}
 }
 
